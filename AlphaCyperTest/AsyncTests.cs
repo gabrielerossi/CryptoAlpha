8296f9a594e9293acfc811a3e0a7626f09b54b72
Gabriele Rossi <gabriele.rossi.97@live.it>
2015-11-04 09:34:07 +0100

    Aggiunta e implementata la classe AsyncTests.

---
+++ AlphaCyperTest/AsyncTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AlphaCypher;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System.Threading.Tasks;
+
+namespace AlphaCyperTest
+{   
+    [TestClass]
+    public class AsyncTests
+    {
+        [TestMethod]
+        public async Task TestEncodeAsync()
+        {
+            string text = "AZZO";
+            string cypher = "DFS";
+            string ris = "DERR";
+            Vigenere crypt = new Vigenere();
+            string encoder = await crypt.EncodeAsync(text, cypher);
+            Assert.AreEqual(encoder, ris);
+        }
+
+        [TestMethod]
+        public async Task TestDecodeAsync()
+        {
+            string text = "DERR";
+            string cypher = "DFS";
+            string ris = "AZZO";
+            Vigenere crypt = new Vigenere();
+            string decoder = await crypt.DecodeAsync(text, cypher);
+            Assert.AreEqual(decoder, ris);
+        }
+    }
+}
